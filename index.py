ac9e1f239c924f94b783b47f951cbf432202a75a
jacky60677 <107839579+jacky60677@users.noreply.github.com>
2022-11-01 23:47:14 +0800

    Update index.py

--- index.py
+++ index.py
@@ -72,12 +72,12 @@ def search():
 	if request.method == "POST":
 		keyword = request.form["keyword"]	
 		Result = ""      
-    	for doc in docs:
+    		for doc in docs:
 			result = doc.to_dict()
 			if keyword in result["Course"]:
 				Result += "課程名稱：" + result["Course"]#+"，教師姓名：" + result["Leacture"]+"，上課時間 : " + result["Time"]+"，在"+result["Room"]+"上課"
-        return Result
-    else:
-        return render_template("search.html")
+        	return Result
+    	else:
+        	return render_template("search.html")
 #if __name__ == "__main__":
 #	app.run()
