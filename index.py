e0227a117040cba275cb20baccc97ed3fa22dda8
jacky60677 <jacky60677@gmail.com>
2022-11-01 23:40:12 +0800

    make it better

--- index.py
+++ index.py
@@ -70,7 +70,7 @@ def search():
 	collection_ref = db.collection("111")
 	docs = collection_ref.get()
 
-	if request.method == "POST":
+		if request.method == "POST":
 
 			keyword = request.form["keyword"]
     	#Cond = input(keyword) 
@@ -83,8 +83,8 @@ def search():
 				if keyword in result["Course"]:
 					Result += "課程名稱：" + result["Course"]#+"，教師姓名：" + result["Leacture"]+"，上課時間 : " + result["Time"]+"，在"+result["Room"]+"上課"
         	return Result
-    else:
-        return render_template("search.html")
+    	else:
+        	return render_template("search.html")
 
 	
 #if __name__ == "__main__":
