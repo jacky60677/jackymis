de8a912323e72ac7c7e4ad83df14f6b2db7abe10
jacky60677 <jacky60677@gmail.com>
2022-11-24 15:40:42 +0800

    make it better

--- index.py
+++ index.py
@@ -81,7 +81,7 @@ def search():
 		#Cond = input(keyword) 
 	
 		Result = ""
-        
+
 		for doc in docs:
 			r = doc.to_dict()
 
@@ -96,54 +96,54 @@ def search():
 
 @app.route("/movienews")
 def movienews():
-    url = "http://www.atmovies.com.tw/movie/next/"
-    Data = requests.get(url)
-    Data.encoding = "utf-8"
-    sp = BeautifulSoup(Data.text, "html.parser")
-    result=sp.select(".filmListAllX li")
-    lastUpdate = sp.find("div", class_="smaller09").text[5:]
+	url = "http://www.atmovies.com.tw/movie/next/"
+	Data = requests.get(url)
+	Data.encoding = "utf-8"
+	sp = BeautifulSoup(Data.text, "html.parser")
+	result=sp.select(".filmListAllX li")
+	lastUpdate = sp.find("div", class_="smaller09").text[5:]
 
-    for item in result:
-        picture = item.find("img").get("src").replace(" ", "")
-        title = item.find("div", class_="filmtitle").text
-        movie_id = item.find("div", class_="filmtitle").find("a").get("href").replace("/", "").replace("movie", "")
-        hyperlink = "http://www.atmovies.com.tw" + item.find("div", class_="filmtitle").find("a").get("href")
-        show = item.find("div", class_="runtime").text.replace("上映日期：", "")
-        show = show.replace("片長：", "")
-        show = show.replace("分", "")
-        showDate = show[0:10]
-        showLength = show[13:]
+	for item in result:
+		picture = item.find("img").get("src").replace(" ", "")
+		title = item.find("div", class_="filmtitle").text
+		movie_id = item.find("div", class_="filmtitle").find("a").get("href").replace("/", "").replace("movie", "")
+		hyperlink = "http://www.atmovies.com.tw" + item.find("div", class_="filmtitle").find("a").get("href")
+		show = item.find("div", class_="runtime").text.replace("上映日期：", "")
+		show = show.replace("片長：", "")
+		show = show.replace("分", "")
+		showDate = show[0:10]
+		showLength = show[13:]
+		images = item.select("img")
+        
+		if len(images) == 1:
+			rate = "目前尚無分級資訊"
+		else:
+			rate = images[1].get("src")
+			if rate == "/images/cer_G.gif":
+				rate = "普遍級(一般觀眾皆可觀賞)"
+			elif rate == "/images/cer_P.gif":
+				rate = "輔導級(未滿十二歲之兒童不得觀賞)"
+			elif rate == "/images/cer_F2.gif":
+				rate = "保護級(未滿六歲之兒童不得觀賞，六歲以上未滿十二歲之兒童須父母、師長或成年親友陪伴輔導觀賞)"
+			elif rate == "/images/cer_F5.gif":
+				rate = "輔導級(未滿十五歲之人不得觀賞)"
+			elif rate == "/images/cer_R.gif":
+				rate = "限制級(未滿十八歲之人不得觀賞)"
+		#print("電影分級:" + rate + "\n")
 
-        images = item.select("img")
-  		if len(images) == 1:
-    		rate = "目前尚無分級資訊"
-  		else:
-    		rate = images[1].get("src")
-    		if rate== "/images/cer_G.gif":
-      			rate = "普遍級(一般觀眾皆可觀賞)"
-    		elif rate == "/images/cer_P.gif":
-      			rate = "輔導級(未滿十二歲之兒童不得觀賞)"
-    		elif rate == "/images/cer_F2.gif":
-      			rate = "保護級(未滿六歲之兒童不得觀賞，六歲以上未滿十二歲之兒童須父母、師長或成年親友陪伴輔導觀賞)"
-    		elif rate == "/images/cer_F5.gif":
-      			rate = "輔導級(未滿十五歲之人不得觀賞)"
-    		elif rate == "/images/cer_R.gif":
-      			rate = "限制級(未滿十八歲之人不得觀賞)"
-  		#print("電影分級:" + rate + "\n")
+		doc = {
+			"title" : title ,
+			"rate" : rate ,
+			"picture" : picture ,
+			"hyperlink" : hyperlink ,
+			"showDate" : showDate ,
+			"showLength" : showLength ,
+			"lastUpdate" : lastUpdate 
+		}
 
-        doc = [
-            "title": title,
-            "rate": rate,
-            "picture": picture,
-            "hyperlink": hyperlink,
-            "showDate": showDate,
-            "showLength": showLength,
-            "lastUpdate": lastUpdate
-        ]
-
-        doc_ref = db.collection("丞彥電影").document(movie_id)
-        doc_ref.set(doc)
-	return "近期上映電影已爬蟲及存檔完畢，網站最近更新日期為：" + lastUpdate 
+		doc_ref = db.collection("丞彥電影").document(movie_id)
+		doc_ref.set(doc)
+	return "近期上映電影已爬蟲及存檔完畢，網站最近更新日期為：" + lastUpdate
 
 
 @app.route("/movie",methods=["GET", "POST"])
@@ -156,8 +156,8 @@ def movie():
 		rate = ""
 		for doc in docs:
 			r=doc.to_dict()
-			if MovieTitle in r["片名"]:
-				info +=  "片名：<a href=" + r["hyperlink"] + ">" + r["片名"] + "</a><br>"
+			if MovieTitle in r["title"]:
+				info +=  "片名：<a href=" + r["hyperlink"] + ">" + r["title"] + "</a><br>"
 				info += "海報：" + r["picture"] + "<br>"
 				info += "片長：" + r["showLength"] + " 分鐘<br>"
 				info += "上映日期：" + r["showDate"] + "<br><br>"
@@ -166,5 +166,5 @@ def movie():
 	else:  
 		return render_template("input.html")
 
-#if __name__ == "__main__":
-#	app.run()
+if __name__ == "__main__":
+	app.run()
