0f1c1c036d99a8b49d28ab70a5fc80d989df0b06
jacky60677 <jacky60677@gmail.com>
2022-11-29 11:34:43 +0800

    make it better

--- index.py
+++ index.py
@@ -190,7 +190,7 @@ def webhook():
             rate = "輔導級(未滿十五歲之人不得觀賞)"
         info = "您選擇的電影分級是：" + rate + "，相關電影：\n"
 
-        collection_ref = db.collection("子青電影")
+        collection_ref = db.collection("丞彥電影")
         docs = collection_ref.get()
         result = ""
         for doc in docs:
@@ -202,5 +202,5 @@ def webhook():
 
     return make_response(jsonify({"fulfillmentText": info}))
 
-# if __name__ == "__main__":
-	# app.run()
+if __name__ == "__main__":
+	app.run()
