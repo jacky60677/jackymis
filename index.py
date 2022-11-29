a37be9450b89d093f28b96d4639149263cf33d47
jacky60677 <jacky60677@gmail.com>
2022-11-29 18:21:07 +0800

    make it better

--- index.py
+++ index.py
@@ -191,7 +191,7 @@ def webhook():
 				result += "介紹：" + r["hyperlink"] + "\n\n"
 		info += result
 '''
-		return make_response(jsonify({"fulfillmentText": info}))
+	return make_response(jsonify({"fulfillmentText": info}))
 
 #if __name__ == "__main__":
 	#app.run()
