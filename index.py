ff27bfbfb227250edf3da2f9ed8ed27aad784a33
jacky60677 <jacky60677@gmail.com>
2022-11-29 18:24:48 +0800

    make it better

--- index.py
+++ index.py
@@ -174,13 +174,13 @@ def webhook():
 	req = request.get_json(force=True)
 	if (action == "rateChoice"):
 		rate = req.get("queryResult").get("parameters").get("rate")
-			if (rate == "輔12級"):
-				rate = "輔導級(未滿十二歲之兒童不得觀賞)"
-			elif (rate == "輔15級"):
-				rate = "輔導級(未滿十五歲之人不得觀賞)"
+		if (rate == "輔12級"):
+			rate = "輔導級(未滿十二歲之兒童不得觀賞)"
+		elif (rate == "輔15級"):
+			rate = "輔導級(未滿十五歲之人不得觀賞)"
 
 		info += "您選擇的電影分級是：" + rate + "，相關電影：\n"
-'''
+
 		collection_ref = db.collection("丞彥電影")
 		docs = collection_ref.get()
 		result = ""
@@ -190,8 +190,8 @@ def webhook():
 				result += "片名：" + r["title"] + "\n"
 				result += "介紹：" + r["hyperlink"] + "\n\n"
 		info += result
-'''
+
 	return make_response(jsonify({"fulfillmentText": info}))
 
-#if __name__ == "__main__":
-	#app.run()
+if __name__ == "__main__":
+	app.run()
